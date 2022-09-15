61cd10da23ad61b7526d6de90c357888a04a5c8b
Tsering Tsering <dtsering2@gmail.com>
2022-09-15 16:15:04 -0400

    main page calendar fully rendered (without data)

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -2,6 +2,7 @@ import "../css/MainPage.css"
 import UserCalendar from "./UserCalendar"
 import { useState, useEffect } from "react"
 import loggedInUserIcon from "../logos/loggedinuser.png"
+import smallerLogo from "../logos/smallerLogo.png"
 const MainPage = () => {
     const[userEvents, setUserEvents] = useState ([])
 
@@ -16,20 +17,31 @@ const MainPage = () => {
     return(
         <>
             <div id = "header">
-                <button>Logout</button>
+                <div id = "logoHolder">
+                    <img src = {smallerLogo}></img>
+                    <h5>DoWork</h5>
+                </div>
+                <button id = "logoutBtn">Logout</button>
             </div>
             <div id="sideNavBar">
                 <div id = "sideWelcomeUser">
                     <img src = {loggedInUserIcon} alt = "defaultUserLogo"></img>
                     <h2 id="welcomeGreeting">Hello, Bobby Infanto Valeinte!</h2>
                 </div>
-                <div id = "calendarTags">
+                <div id = "userCalendarTagsContainer">
                     <h3 className = "sideBarCat">Tags</h3> 
+                    <hr className= "underline"></hr>
+                        <div id = "userTags">
+                            <ul><label><input type = "checkBox" className = "checkBox"></input>Holidays</label></ul>
+                            <ul><label><input type = "checkBox" className = "checkBox"></input>Holidays</label></ul>
+                            <ul><label><input type = "checkBox" className = "checkBox"></input>Holidays</label></ul>
+                        </div>
                     {/*TODO: insert the mapped array of all tags created by the user*/}
                     {/*No button to create tags. That functionality will be handle by the create event in which the user will be able to create a new tag for an event if they wish */}
                 </div>
                 <div id = "sideBarGroups">
                     <h3 className = "sideBarCat">Groups</h3>
+                    <hr className= "underline"></hr>
                     {/*TODO: insert the mapped array of all groups that a user belongs to created by the user (we need some special way to notifying user that he created this group*/}
                     {/*TODO:Create the form to create new group*/}
                 </div>
