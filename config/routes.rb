0d3211c782beb67398aa8ae54196eb5ec79380c7
Kevin <magavakevin10@gmail.com>
2022-09-16 10:33:57 -0400

    Updated routes, added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  
 
   #FIXME: To delete the routes below 
   resources :tasks
   get "/users", to: "users#index"
   
+  # JOINER ROUTES
   resources :tasks_joiners
-
+  resources :group_events
   resources :user_events
-
   resources :group_joiners
 
+  # MAIN ROUTES
   resources :users, only:[:update, :show, :destroy, :create] do
     resources :calendars
     resources :groups
@@ -17,15 +19,21 @@ Rails.application.routes.draw do
     resources :tasks
   end
 
-  resources :groups
+  resources :groups do
+    resources :events
+    resources :tasks
+    resources :users
+  end
 
-  resources :events
+  resources :events do
+    resources :groups
+    resources :users
+  end
 
-
+  # USER AUTHORIZATION CONTROLS
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
-  
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
 
