b14fa3b354d79dc79b2332c0258bb6e2efd5f840
Tsering Tsering <dtsering2@gmail.com>
2022-09-16 12:53:15 -0400

    static group tasks created along with form, css is missing for just the inputs and styling for the task itself

--- client/src/components/GroupTasks.jsx
+++ client/src/components/GroupTasks.jsx
@@ -11,9 +11,33 @@ export default function GroupTasks () {
         <div id = "groupTask" style = {toggleCollapse? {"margin-top": "32%"}: {"margin-top": "15%"}}>
             <button id = "collapsible" onClick = {handleToggleTasks}>Group Tasks</button>
             <div id = {toggleCollapse ? "groupTaskContainerShow" : "groupTaskContainerHide"}>
-                <div className = "taskCard"></div>
-                <div id = "taskForm" className = "taskCard"></div>
-                <div id = "taskForm" className = "taskCard"></div>
+                <div id = "taskFormContainer" className = "taskCard">
+                    <form id = "taskForm">
+                        <input className = "taskFormTitle" type = "text" placeholder="Task Title"></input>
+                        <input className = "taskFormDescription" type = "text" placeholder = "Task Description"></input>
+                        <input className = "taskFormStartDate" type = "date" placeholder="Start Date"></input>
+                        <input className = "taskFormEndDate"type = "date" placeHolder="End Date"></input>
+                        <button>Add task</button>
+                    </form>
+                </div>
+                <div id = "taskFormContainer" className = "taskCard">
+                    <form id = "taskForm">
+                        <input className = "taskFormTitle" type = "text" placeholder="Task Title"></input>
+                        <input className = "taskFormDescription" type = "text" placeholder = "Task Description"></input>
+                        <input className = "taskFormStartDate" type = "date" placeholder="Start Date"></input>
+                        <input className = "taskFormEndDate"type = "date" placeHolder="End Date"></input>
+                        <button>Add task</button>
+                    </form>
+                </div>
+                <div className = "taskCard">
+                    <div className = "task">
+                        <h7>Some Task Title</h7>
+                        <h8>Start Date: 12/06/2023</h8>
+                        <h8>End Date: 12/08/2023</h8>
+                        <p>akjsdhfaksjdfhalksdjfhalksdjfhalksjdhfaskljdfhaskldjfhasdkljfhasdlkfjahsdflkajsdhfa</p>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
