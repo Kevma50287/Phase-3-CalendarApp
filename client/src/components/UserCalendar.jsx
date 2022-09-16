7dddb69ff13243d941c50c9bd4ea04dcb052ab7a
Kevin <magavakevin10@gmail.com>
2022-09-15 23:40:28 -0400

    Added AddEvent

--- client/src/components/UserCalendar.jsx
+++ client/src/components/UserCalendar.jsx
@@ -8,8 +8,53 @@ import "../css/UserCalendar.css"
 import { useState } from 'react'
 
 const UserCalendar = ({userEvents}) => {
+    //STATE
+    const initialState = {
+        title:"",
+        description:"",
+        startDate:"",
+        startTime:"",
+        endDate:"",
+        endTime:"",
+        allDay:false
+    }
     const [toggleForm, setToggleForm] = useState(false)
-    console.log(toggleForm)
+    const [eventDetails, setEventDetails] = useState(initialState)
+
+    // HANDLERS
+    const handleAddEvent = (e) => {
+        e.preventDefault()
+        const eventObj = {
+            title: eventDetails.title,
+            description: eventDetails.title,
+            start: `${eventDetails.startDate}T${eventDetails.startTime}`,
+            end: `${eventDetails.endDate}T${eventDetails.endTime}`,
+            allDay: eventDetails.allDay
+        }
+        fetch("/events", {
+            method:"POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(eventObj)
+        })
+
+    }
+
+    const handleEventDetails = (e) => {
+        const {name, value} = e.target
+        setEventDetails(
+            {...eventDetails, [name]:value}
+        )
+    }
+
+    // FIXME:To delete console log
+    console.log(eventDetails)
+
+    const handleCancel = () => {
+        setEventDetails(initialState)
+        setToggleForm(false)
+    }
 
     return (
         <div className = "userCalendarContainer">
@@ -32,13 +77,77 @@ const UserCalendar = ({userEvents}) => {
                     {
                         addEventButton: {
                             text: 'Add Event',
-                            click: () => setToggleForm(e => !e)
+                            click: () => setToggleForm(true)
                         }
                     }
                 }
                 
             />
+
+            {/* TODO: Need to add CSS. Want the form to hover in the middle of the screen with a drop shadow */}
+            <div style={{display: toggleForm ? 'block' : 'none' }}>
+                <form id ="addEventForm" onSubmit={handleAddEvent} >
+                    <h3>Add Event Details</h3>
+                    <input
+                        type = "text"
+                        className = "eventDetailsFormInputs"
+                        placeholder = "Title"
+                        name = "title"
+                        value = {eventDetails.title}
+                        onChange = {handleEventDetails}
+                    ></input>
+                    <textarea
+                        className = "eventDetailsFormInputs"
+                        placeholder = "Add description..."
+                        name = "description"
+                        value = {eventDetails.description}
+                        onChange = {handleEventDetails}
+                    ></textarea>
+                    <input
+                        type = "date"
+                        className = "eventDetailsFormInputs"
+                        placeholder = "Enter Start Date"
+                        name = "startDate"
+                        value = {eventDetails.startDate}
+                        onChange = {handleEventDetails}
+                    ></input>
+                    <input
+                        type = "time"
+                        className = "eventDetailsFormInputs"
+                        placeholder = "Enter Start Time"
+                        name = "startTime"
+                        value = {eventDetails.startTime}
+                        onChange = {handleEventDetails}
+                    ></input>
+                    <input
+                        type = "date"
+                        className = "eventDetailsFormInputs"
+                        placeholder = "Enter End Date"
+                        name = "endDate"
+                        value = {eventDetails.endDate}
+                        onChange = {handleEventDetails}
+                    ></input>
+                    <input
+                        type = "time"
+                        className = "eventDetailsFormInputs"
+                        placeholder = "Enter End Time"
+                        name = "endTime"
+                        value = {eventDetails.endTime}
+                        onChange = {handleEventDetails}
+                    ></input>
+                    <input 
+                        type="checkbox" 
+                        className = "eventDetailsFormInputs"
+                        id="allDay" 
+                        name="allDay" 
+                        value={eventDetails.allDay} 
+                        onChange = {handleEventDetails} /> All Day Event?
+                    <button id = "addEventBtn" type="submit">Add Event</button>
+                </form>
+                <button id = "cancelEventBtn" onClick={handleCancel}>Cancel</button>
+            </div>
         </div>
+        
     
     );
 }
